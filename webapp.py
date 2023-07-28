b3d92c42d071ade239171557c16e71f06ca3d623
sasisftb <116105247+sasisftb@users.noreply.github.com>
2023-07-28 15:59:10 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.header('Sasi GM')
 with st.chat_message("user"):
->>>    st.write("Hello 👋")
->>>    st.line_chart(np.random.randn(30, 3))
+    st.write("Hello 👋")
+    st.line_chart(np.random.randn(30, 3))
 
 # Display a chat input widget.
->>> st.chat_input("Say something")
+ st.chat_input("Say something")
