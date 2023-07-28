9d27ccbf4b1d67ce62aadcad28ef6a7a92b23e26
sasisftb <116105247+sasisftb@users.noreply.github.com>
2023-07-28 16:21:40 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,10 +2,24 @@ import streamlit as st
 import numpy as np
 st.header('Sasi GM')
 with st.chat_message("user"):
-    st.write("Hello 👋")
+    st.write("Hello How may i help you 👋")
     st.line_chart(np.random.randn(30, 3))
 
 # Display a chat input widget.
 prompt = st.chat_input("Say something")
 if prompt:
     st.write(f"User has sent the following prompt: {prompt}")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
